708841eb492a7bc55b16cc2cd50b88f138bac582
Yassine Chbani <yassinec@bam.tech>
2018-10-06 12:44:00 +0200

    Add tab navigation

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,40 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup name generator', home: RandomWords());
+    return MaterialApp(home: TabBarDemo());
+  }
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter demo'),
+        ),
+        body: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              body: TabBarView(
+                children: [
+                  RandomWords(),
+                  Icon(Icons.directions_transit),
+                  Icon(Icons.directions_bike),
+                ],
+              ),
+              bottomNavigationBar: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_transit)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                ],
+                labelColor: Colors.lightBlue,
+                unselectedLabelColor: Colors.black,
+              ),
+            ),
+          ),
+        ));
   }
 }
