8fee34a4319128b0ca21c49382e6357a3a0f2bb1
Yassine Chbani <yassine.chbani@gmail.com>
2019-06-30 13:49:45 +0200

    :sparkles: (ComicPage) Add loader

--- lib/ComicPage.dart
+++ lib/ComicPage.dart
@@ -12,19 +12,36 @@ class ComicPage extends StatefulWidget {
 
 class _ComicPageState extends State<ComicPage> {
   var _imageUrl = "";
+  var _isLoading = false;
 
   Future<void> _fetchComic() async {
+    setState(() {
+      _isLoading = true;
+    });
     final Comic comic = await ComicsApiService.fetchComic();
     setState(() {
       _imageUrl = comic.url;
+      _isLoading = false;
     });
+  }
+
+  Widget _buildComicImage() {
+    return (_imageUrl != "")
+        ? Image.network(_imageUrl)
+        : Text("Load a comic by pressing the below button");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-            Center(child: (_imageUrl != "") ? Image.network(_imageUrl) : null),
+        body: Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            alignment: Alignment(0, 0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _isLoading ? CircularProgressIndicator() : _buildComicImage()
+                ])),
         floatingActionButton: FloatingActionButton.extended(
             label: Text("Load comic"), onPressed: _fetchComic));
   }
