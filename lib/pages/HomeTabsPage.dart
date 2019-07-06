586799afafa3f8837fc47e3479ae0b67e22eb5ad
Yassine Chbani <yassine.chbani@gmail.com>
2019-07-06 19:27:12 +0200

    fix

--- lib/pages/HomeTabsPage.dart
+++ lib/pages/HomeTabsPage.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/RandomWordsPage.dart';
 import 'package:flutter_app/pages/ComicPage.dart';
@@ -8,6 +6,7 @@ import 'package:flutter_app/components/CenteredButton.dart';
 class HomeTabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Function navigateToMap = () => Navigator.pushNamed(context, "/map");
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter demo'),
@@ -15,15 +14,16 @@ class HomeTabsPage extends StatelessWidget {
         body: SafeArea(
             child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: TabsWidget(),
+          home: TabsWidget(navigateToMap: navigateToMap),
         )));
   }
 }
 
 class TabsWidget extends StatelessWidget {
+  const TabsWidget({Key key, @required this.navigateToMap}) : super(key: key);
+  final Function navigateToMap;
   @override
   Widget build(BuildContext context) {
-    Function navigateToMap = () => Navigator.pushNamed(context, "/map");
     return DefaultTabController(
       length: 3,
       child: Scaffold(
